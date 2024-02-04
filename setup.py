6348df2953c49d8f0e7018862441cc9173516c76
Aditya Kumar <theadityakumardeveloper@gmail.com>
2024-02-04 22:34:10 +0530

    Initial Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mailbridge',
+    version='1',
+    packages=[''],
+    url='https://github.com/adityajideveloper/mailbridge/',
+    license='MIT',
+    author='Aditya Kumar',
+    author_email='theadityakumardeveloper@gmail.com',
+    description=''
+)
